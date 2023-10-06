f6e7adcf47c2595605373710cf713fdf2a2dc509
Liidia Ilves <liidia_ilves@hotmail.com>
2023-10-06 12:56:22 +0300

    grrlpowercomic.com naide

--- main.js
+++ main.js
@@ -5,30 +5,31 @@ import fs from "fs";
 function delay(time) {
     return new Promise(resolve => setTimeout(resolve, time));
   } 
+    await delay(1000);
+
+let url = 'https://www.grrlpowercomic.com/archives/comic/grrl-power-1194/';
 
 for(let i = 1; i<10; i++) {
     let data;
     let response;
     if (!fs.existsSync('./cache/' + i + '.html')) {
-        response = await axios.get('https://xkcd.com/' + i); 
+        response = await axios.get(url); 
         await delay(1000);
         console.log('NOT CACHE');
         data = response.data;
-        fs.writeFileSync('./cache/' + i + '.html', data)
-        
+        fs.writeFileSync('./cache/' + i + '.html', data)   
     }
 
     else {
         console.log('CACHE!!!');
-        data = fs.readFileSync('./cache/'+ i + '.html');
-        
+        data = fs.readFileSync('./cache/'+ i + '.html');  
     }
 
     const $ = cheerio.load(data);
-    let img = $('#comic>img');
+    let img = $('#comic img'); // kuskil id comic sees on img
     console.log(img.attr('title'));
-    console.log(img.attr('alt'));
     console.log(img.attr('src'));
-    await delay(1000);
+    url = $('.comic-nav-previous').eq(0).attr('href');
+    console.log(url);
 }
 
