5da1eb280c5140b3f5a0c745f38bd1e0e18d7e90
Liidia Ilves <liidia_ilves@hotmail.com>
2023-10-06 15:30:51 +0300

    terminali graafika

--- main.js
+++ main.js
@@ -1,35 +1,55 @@
-import axios from "axios";
-import * as cheerio from "cheerio";
-import fs from "fs";
+function write(text){
+    process.stdout.write(text);
+}
+write('\x1b[0;30mHello world\n'); // Set style to bold, black foreground, pole näha
+write('\x1b[0;31mHello world\n'); 
+write('\x1b[0;32mHello world\n'); 
+write('\x1b[0;33mHello world\n'); 
+write('\x1b[0;34mHello world\n'); 
+write('\x1b[0;35mHello world\n'); 
+write('\x1b[0;36mHello world\n'); 
+write('\x1b[0;37mHello world\n'); 
+write('\x1B[1mHello world\n'); 
+write('\x1B[0m'); //reset
+write('\x1b[0;40mHello world\n'); 
+write('\x1b[0;41mHello world\n'); 
+write('\x1b[0;42mHello world\n'); 
+write('\x1b[0;43mHello world\n'); 
+write('\x1b[0;44mHello world\n'); 
+write('\x1b[0;45mHello world\n'); 
+write('\x1b[0;46mHello world\n'); 
+write('\x1b[0;47mHello world\n'); 
+write('\x1b[0;44mHello world\x1B[0;0m\n'); // see resetib rea lõpus värvid 
+write('\x1b[0;41m');
+write('Hello there!');
+write('\x1B[0;0m');
+write('\n');
 
-function delay(time) {
-    return new Promise(resolve => setTimeout(resolve, time));
-  } 
-    await delay(1000);
+for( let i = 0; i<255; i++){
+    write(`\x1B[38;5;${i}m 1`)
+};
+for( let i = 0; i<255; i++){
+    write(`\x1B[48;5;${i}m `)
+};
+for( let i = 0; i<255; i++){
+    write(`\x1B[48;2;${i};${i};${i}m `)
+}; // truecolors
 
-let url = 'https://tahvel.edu.ee/hois_back/timetableevents/timetableByGroup/14';
+write('Hello world');
+setTimeout(() => {
+    write('\x1B[5D');
+    write('people');
+}, 1000);
 
-let res = await axios.get(url, {
-    params: {
-        from: '2023-10-06T00:00:00Z', // võib ka anda varasema kuupäeva!
-        studentGroups: 7225,
-        thru: '2023-10-06T00:00:00Z'
-    }
-});
+let date = new Date();
+write(date.toLocaleTimeString());
+setInterval(() => {
+    write('\x1B[10D'); // kustutab tagasi 10 märki
+    let date = new Date();
+    write(date.toLocaleTimeString()); // asendab
+}, 1000); // NB! võtab arvuti kella! Kui inglise oma, siis tekib probleem 10-12 tundidega!
 
-let current = new Date();
 
-res.data.timetableEvents.forEach((event) => {
-    let eventStart = new Date();
-    eventStart.setHours(event.timeStart.substring(0,2), event.timeStart.substring(3,5),0);
 
-    let eventEnd = new Date();
-    eventEnd.setHours(event.timeEnd.substring(0,2), event.timeEnd.substring(3,5), 0);
-
-    if(eventStart < current && eventEnd > current) {
-        console.log(`Sa pead olema klassis ${event.rooms[0].roomCode}, kus sul on tund ${event.nameEt} õpetaja ${event.teachers[0].name}!`);
-    }
-});
-// console.log(res.data);
 
 
