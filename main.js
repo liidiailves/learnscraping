77217e7d28040b0691889d5339fb12fe2ea405ce
Liidia Ilves <liidia_ilves@hotmail.com>
2023-10-06 13:52:31 +0300

    tahvel timetable

--- main.js
+++ main.js
@@ -7,29 +7,29 @@ function delay(time) {
   } 
     await delay(1000);
 
-let url = 'https://www.grrlpowercomic.com/archives/comic/grrl-power-1194/';
+let url = 'https://tahvel.edu.ee/hois_back/timetableevents/timetableByGroup/14';
 
-for(let i = 1; i<10; i++) {
-    let data;
-    let response;
-    if (!fs.existsSync('./cache/' + i + '.html')) {
-        response = await axios.get(url); 
-        await delay(1000);
-        console.log('NOT CACHE');
-        data = response.data;
-        fs.writeFileSync('./cache/' + i + '.html', data)   
+let res = await axios.get(url, {
+    params: {
+        from: '2023-10-06T00:00:00Z', // võib ka anda varasema kuupäeva!
+        studentGroups: 7225,
+        thru: '2023-10-06T00:00:00Z'
     }
+});
 
-    else {
-        console.log('CACHE!!!');
-        data = fs.readFileSync('./cache/'+ i + '.html');  
+let current = new Date();
+
+res.data.timetableEvents.forEach((event) => {
+    let eventStart = new Date();
+    eventStart.setHours(event.timeStart.substring(0,2), event.timeStart.substring(3,5),0);
+
+    let eventEnd = new Date();
+    eventEnd.setHours(event.timeEnd.substring(0,2), event.timeEnd.substring(3,5), 0);
+
+    if(eventStart < current && eventEnd > current) {
+        console.log(`Sa pead olema klassis ${event.rooms[0].roomCode}, kus sul on tund ${event.nameEt} õpetaja ${event.teachers[0].name}!`);
     }
+});
+// console.log(res.data);
 
-    const $ = cheerio.load(data);
-    let img = $('#comic img'); // kuskil id comic sees on img
-    console.log(img.attr('title'));
-    console.log(img.attr('src'));
-    url = $('.comic-nav-previous').eq(0).attr('href');
-    console.log(url);
-}
 
